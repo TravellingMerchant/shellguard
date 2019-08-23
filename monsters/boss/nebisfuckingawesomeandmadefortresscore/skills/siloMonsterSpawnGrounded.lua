5930944e5eba8ce315f95cdaebc9674f23c7f6dc
Nebulox <42601049+Nebulox@users.noreply.github.com>
2019-08-24 09:24:58 +1000

    - General Updates
    - Silo attack is functional

--- monsters/boss/nebisfuckingawesomeandmadefortresscore/skills/siloMonsterSpawnGrounded.lua
+++ monsters/boss/nebisfuckingawesomeandmadefortresscore/skills/siloMonsterSpawnGrounded.lua
@@ -5,13 +5,26 @@ function siloMonsterSpawnGrounded.enter()
   if not hasTarget() then return nil end
   
   self.active = false
+  self.finished = false
   self.canFire = false
   self.leftSiloOffset = config.getParameter("siloMonsterSpawnGrounded.leftSiloOffset")
   self.rightSiloOffset = config.getParameter("siloMonsterSpawnGrounded.rightSiloOffset")
-
+  self.monsterLevel = monster.level()
+  
   return {
     fireDuration = config.getParameter("siloMonsterSpawnGrounded.fireDuration", 1),
-    windupDuration = config.getParameter("siloMonsterSpawnGrounded.windupDuration", 1)
+    windupDuration = config.getParameter("siloMonsterSpawnGrounded.windupDuration", 1),
+	monsterCount = config.getParameter("siloMonsterSpawnGrounded.monsterCount", 5),
+	monsters = config.getParameter("siloMonsterSpawnGrounded.monsters"),
+    monsterTypes = config.getParameter("siloMonsterSpawnGrounded.monsterTypes"),
+    monsterCount = config.getParameter("siloMonsterSpawnGrounded.monsterCount"),
+    monsterTestPoly = config.getParameter("siloMonsterSpawnGrounded.monsterTestPoly"),
+    spawnOnGround = config.getParameter("siloMonsterSpawnGrounded.spawnOnGround"),
+    spawnAnimation = config.getParameter("siloMonsterSpawnGrounded.spawnAnimation"),
+    spawnRangeX = config.getParameter("siloMonsterSpawnGrounded.spawnRangeX"),
+    spawnRangeY = config.getParameter("siloMonsterSpawnGrounded.spawnRangeY"),
+    spawnTolerance = config.getParameter("siloMonsterSpawnGrounded.spawnTolerance"),
+    spawnAnimationStatus = config.getParameter("siloMonsterSpawnGrounded.spawnAnimationStatus")
   }
 end
 
@@ -29,36 +42,92 @@ function siloMonsterSpawnGrounded.update(dt, stateData)
   if self.active and stateData.windupDuration > 0 and not self.canFire then
     stateData.windupDuration = stateData.windupDuration - dt
 	
-	if stateData.windupDuration <= 0 then
+	if stateData.windupDuration <= 0 and not self.canFire then
       self.canFire = true
 	end
   end
   
-  if self.active and self.canFire and stateData.fireDuration > 0 then
+  if self.active and stateData.fireDuration > 0 and not self.finished then
     stateData.fireDuration = stateData.fireDuration - dt
   
-	if animator.animationState("bottomLeftSilo") == "risen" then
+	if animator.animationState("bottomLeftSilo") == "risen" and not self.canFire then
 	  animator.setAnimationState("bottomLeftSilo", "dooropen")
-	  chosenMonsters = {}
-	  for _ = 1, 5 do
-		table.insert(chosenMonsters, monsters[math.random(#monsters)])
-	  end
+
+	  for i = 1, math.random(stateData.monsterCount[1], stateData.monsterCount[2]) do
+	    --Calculate initial x and y offset for the spawn position
+	    local xOffset = math.random((self.leftSiloOffset[1] - stateData.spawnRangeX), self.leftSiloOffset[1])
+	    xOffset = xOffset * util.randomChoice({-1, 1})
+	    local yOffset = math.random(self.leftSiloOffset[2], stateData.spawnRangeY)
+	    local position = vec2.add(entity.position(), {xOffset, yOffset})
 	  
-	  for _, monster in ipairs(chosenMonsters) do
-		world.spawnMonster(monster, self.leftSiloOffset)
+	    --Optionally correct the position by finding the ground below the projected position
+	    local correctedPositionAndNormal = {position, nil}
+	    if stateData.spawnOnGround then
+	  	  correctedPositionAndNormal = world.lineTileCollisionPoint(position, vec2.add(position, {0, -50})) or {position, 0}
+	    end
+	  
+	    --Resolve the monster poly collision to ensure that we can place an monster at the designated position
+	    local resolvedPosition = world.resolvePolyCollision(stateData.monsterTestPoly, correctedPositionAndNormal[1], stateData.spawnTolerance)
+	  
+	    if resolvedPosition then
+		  --Spawn the monster and optionally force the monster spawn effect on them
+		  local entityId = world.spawnMonster(util.randomChoice(stateData.monsterTypes), resolvedPosition, {level = self.monsterLevel, aggressive = true})
+		  if stateData.spawnAnimation then
+		    world.callScriptedEntity(entityId, "status.addEphemeralEffect", stateData.spawnAnimationStatus)
+		  end
+	    end
 	  end
+	  self.canFire = false
+	  self.finished = true
 	end
-	if animator.animationState("bottomRightSilo") == "risen" then
+	if animator.animationState("bottomRightSilo") == "risen" and not self.canFire then
+	  animator.setAnimationState("bottomRightSilo", "dooropen")
+
+	  for i = 1, math.random(stateData.monsterCount[1], stateData.monsterCount[2]) do
+	    --Calculate initial x and y offset for the spawn position
+	    local xOffset = math.random(self.rightSiloOffset[1], (self.rightSiloOffset[1] + stateData.spawnRangeX))
+	    xOffset = xOffset * util.randomChoice({-1, 1})
+	    local yOffset = math.random(self.rightSiloOffset[2], stateData.spawnRangeY)
+	    local position = vec2.add(entity.position(), {xOffset, yOffset})
+	  
+	    --Optionally correct the position by finding the ground below the projected position
+	    local correctedPositionAndNormal = {position, nil}
+	    if stateData.spawnOnGround then
+	  	  correctedPositionAndNormal = world.lineTileCollisionPoint(position, vec2.add(position, {0, -50})) or {position, 0}
+	    end
+	  
+	    --Resolve the monster poly collision to ensure that we can place an monster at the designated position
+	    local resolvedPosition = world.resolvePolyCollision(stateData.monsterTestPoly, correctedPositionAndNormal[1], stateData.spawnTolerance)
+	  
+	    if resolvedPosition then
+		  --Spawn the monster and optionally force the monster spawn effect on them
+		  local entityId = world.spawnMonster(util.randomChoice(stateData.monsterTypes), resolvedPosition, {level = self.monsterLevel, aggressive = true})
+		  if stateData.spawnAnimation then
+		    world.callScriptedEntity(entityId, "status.addEphemeralEffect", stateData.spawnAnimationStatus)
+		  end
+	    end
+	  end
+	  self.canFire = false
+	  self.finished = true
 	end
 	
-	if stateData.fireDuration <= 0 then
+	if self.finished then
 	  if animator.animationState("bottomRightSilo") == "openidle" then
 	    animator.setAnimationState("bottomRightSilo", "doorclose")
 	  end
 	  if animator.animationState("bottomLeftSilo") == "openidle" then
 	    animator.setAnimationState("bottomLeftSilo", "doorclose")
 	  end
-	  return true
+	  if animator.animationState("bottomRightSilo") == "risen" then
+	    animator.setAnimationState("bottomRightSilo", "sink")
+	  end
+	  if animator.animationState("bottomLeftSilo") == "risen" then
+	    animator.setAnimationState("bottomLeftSilo", "sink")
+	  end
+	  
+	  if animator.animationState("bottomLeftSilo") == "idle" and animator.animationState("bottomLeftSilo") == "idle" then
+	    return true
+	  end
 	end
   end
 
