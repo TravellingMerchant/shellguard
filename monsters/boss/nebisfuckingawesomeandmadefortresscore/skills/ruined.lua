b8d676ef1605550f7f5ce1c12cb0518297e79cc4
Nebulox <42601049+Nebulox@users.noreply.github.com>
2019-09-17 10:24:05 +1000

    Minor Updates
    - Added 2 debug macros, /fortressfight which transports you to fortress' boss room, and /shellguardhub which takes you to the shellguard outpost
    - Refined animations for spearguns

--- monsters/boss/nebisfuckingawesomeandmadefortresscore/skills/ruined.lua
+++ monsters/boss/nebisfuckingawesomeandmadefortresscore/skills/ruined.lua
@@ -15,13 +15,11 @@ function ruined.enteringState(stateData)
   world.spawnVehicle(stateData.vehicleType, vec2.add(entity.position(), {-30, -15}))
   animator.setAnimationState("stages", "stage"..currentPhase())
   status.addEphemeralEffect("fortressidontwanttodie")
-end
-
-function ruined.update(dt, stateData)
- --status.setResource("health", status.stat("maxHealth"))
+	
   --Set up a damagelistener for incoming damage
-  local damageListener = damageListener("damageTaken", function(notifications)
+	self.slapsTaken = damageListener("damageTaken", function(notifications)
     for _,notification in pairs(notifications) do
+			sb.logInfo(sb.printJson(notification, 1))
       if notification.damageSourceKind == "ruincleaverfinisher" then
 				status.removeEphemeralEffect("fortressidontwanttodie")
 				status.setResource("health", 0)
@@ -31,9 +29,13 @@ function ruined.update(dt, stateData)
       end
     end
   end)
-  damageListener:update()
+end
+
+function ruined.update(dt, stateData)
+  --status.setResource("health", status.stat("maxHealth"))
+
+  self.slapsTaken:update()
 end
 
 function ruined.leavingState(stateData)
-  animator.rotateGroup("core", 0, true)
 end
