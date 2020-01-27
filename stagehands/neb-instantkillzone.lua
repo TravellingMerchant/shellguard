958a6ff7a1d93cf8272ebe0af90339b93b8c5167
Nebulox <42601049+Nebulox@users.noreply.github.com>
2020-01-27 17:49:11 +1000

    Minor Update
    - Changes how instant kill zone handles killing

--- stagehands/neb-instantkillzone.lua
+++ stagehands/neb-instantkillzone.lua
@@ -1,11 +1,14 @@
 require "/scripts/stagehandutil.lua"
 
 function init()
+  self.validEntityTypes = config.getParameter("validEntityTypes", {"npc", "monster"})
 end
 
 function update(dt)
   --Find all entites in stagehand and kill them
-  local livingEntities = broadcastAreaQuery({includedTypes = {"npc", "monster"}})
+  local livingEntities = broadcastAreaQuery(
+    includedTypes = self.validEntityTypes,
+  )
   for _, entity in pairs(livingEntities) do
 	world.sendEntityMessage(entity, "applyStatusEffect", "monsterdespawn")
 	world.sendEntityMessage(entity, "despawn")
