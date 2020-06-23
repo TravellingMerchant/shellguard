7553fbc38d53a17b253e2a506db78861859083c2
Nebulox <42601049+Nebulox@users.noreply.github.com>
2020-06-23 23:35:21 +1000

    Lord of Chompers, Archchomper, Chomby
    - Adds the best boi boss
    - He can fly
    - He can teleport
    - He can fucking kill you

---
+++ monsters/boss/sgchomperdragonboss/skills/sgTeleportAttack.lua
@@ -0,0 +1,60 @@
+sgTeleportAttack = {}
+
+function sgTeleportAttack.enter()
+  if self.targetPosition == nil then return nil end
+
+  sgTeleportAttack.reappeared = false
+  animator.burstParticleEmitter("teleport")
+  animator.playSound("teleportOut")
+  return { 
+    timer = config.getParameter("sgTeleportAttack.skillTime")
+  }
+end
+
+function sgTeleportAttack.enteringState(stateData)
+  monster.setActiveSkillName("sgTeleportAttack")
+end
+
+function sgTeleportAttack.update(dt, stateData)
+  mcontroller.controlFace(1)
+  status.addEphemeralEffect("invulnerable")
+
+  if stateData.timer > config.getParameter("sgTeleportAttack.skillTime") - 0.55 then
+    mcontroller.controlFly({ 0, 0 })
+  elseif stateData.timer > 0 then
+    if animator.animationState("movement") ~= "invisible" then
+      animator.setAnimationState("movement", "invisible")
+	  animator.setAnimationState("head", "invisible")
+	  animator.setAnimationState("thrusters", "invisible")
+    end
+
+    if self.targetPosition ~= nil then
+      flyTo({
+        self.targetPosition[1],
+        self.spawnPosition[2]
+      })
+    else
+      mcontroller.controlFly({ 0, 1 })
+    end
+
+    if stateData.timer < 0.3 and not sgTeleportAttack.reappeared then
+      sgTeleportAttack.reappeared = true
+      animator.burstParticleEmitter("teleport")
+      animator.playSound("teleportIn")
+    end
+  else
+    return true
+  end
+
+  stateData.timer = stateData.timer - dt
+  return false
+end
+
+function sgTeleportAttack.leavingState(stateData)
+  status.removeEphemeralEffect("invulnerable")
+  mcontroller.setVelocity({ 0, 0 })
+  mcontroller.controlFly({ 0, 0 })
+  animator.setAnimationState("movement", "visible")
+  animator.setAnimationState("head", "visible")
+  animator.setAnimationState("thrusters", "on")
+end
